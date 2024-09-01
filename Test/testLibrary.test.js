c673d79e15b99e09ed5ec49dc00083bf3a853707
Bhautik Vala <bhautiksinhvala333@gmail.com>
2024-09-01 09:46:26 +0530

    Add first test of adding book -> expecting success-message only

---
+++ Test/testLibrary.test.js
@@ -0,0 +1,21 @@
+
+// Unit Test for Adding book
+describe('Add Book Testing', () => {
+
+    // addBookHelper is Class for performing addBook operation to particular library, declare addBookHelperInstance for storing instance of it.make instance of Libray also.
+    let addBookHelperInstance
+    let libraryInstance
+
+    beforeAll(() => {
+        libraryInstance = new Library
+        // pass instance of Library in constructor so book get added in desired library.
+        addBookHelperInstance = new AddBookHelper(libraryInstance)
+    });
+
+    test('should return successfull message of adding book', () => {
+        const message = addBookHelperInstance.addBook('978-0135166307',"Effective Java","Joshua Bloch","2018")
+        // addBook should return message of type string, if book added successfully
+        expect(message).toBe("Book stored successfully")
+    });
+
+})
