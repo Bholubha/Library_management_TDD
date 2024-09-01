ef4cb3f9a9ae5d8b9885ee2f174a9ad791b55ecd
Bhautik Vala <bhautiksinhvala333@gmail.com>
2024-09-01 11:19:29 +0530

    Add test for testing copy_count field for identical book

--- Test/testLibrary.test.js
+++ Test/testLibrary.test.js
@@ -43,4 +43,14 @@ describe('Add Book Testing', () => {
         expect(()=> addBookHelperInstance.addBook('978-01359570avc',"JavaScript and JQuery","Jon Duckett","2014")).toThrow("Please provide correct ISBN")
     })
 
+    
+    // Test for checking copy_count for same books
+    test('should track number of same copy of identical book',()=>{
+        addBookHelperInstance.addBook('978-1118531648',"JavaScript: The Definitive Guide","David Flanagan","2020")
+        addBookHelperInstance.addBook('978-1118531648',"JavaScript: The Definitive Guide","David Flanagan","2020")
+
+        // after adding two identical book copy_count must be 2
+        expect(libraryInstance.getAllBooks()['978-1118531648'].copy_count).toBe(2)
+    })
+
 })
